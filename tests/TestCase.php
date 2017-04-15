23a430b1560a7829ed41b9bd0526ebaedcd3b4f5
Coen Zimmerman <coen@pixelindustries.com>
2017-04-15 19:47:13 +0200

    Cleaned up testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,32 +24,4 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         ]);
     }
 
-    /**
-     * Performs simple assertion of validity of JSON-API serialized data.
-     *
-     * @param mixed|array     $data
-     * @param string          $type
-     * @param string          $id
-     * @param array           $attributes   if nonempty, attributes to match the values for
-     */
-    protected function assertBasicJsonApiResponse($data, $type, $id, $attributes = [])
-    {
-        static::assertInternalType('array', $data);
-        static::assertArrayHasKey('data', $data);
-
-        static::assertArrayHasKey('type', $data['data'], "Data has no 'type' key");
-        static::assertEquals($type, $data['data']['type']);
-
-        static::assertArrayHasKey('id', $data['data'], "Data has no 'id' key");
-        static::assertSame($id, $data['data']['id']);
-
-        if ( ! empty($attributes)) {
-            static::assertArrayHasKey('attributes', $data['data']);
-
-            foreach ($attributes as $key => $value) {
-                static::assertArrayHasKey($key, $data['data']['attributes'], "Attributes does not have key '{$key}''");
-                static::assertSame($value, $data['data']['attributes'][$key], "Attribute value for {$key} is incorrect");
-            }
-        }
-    }
 }
