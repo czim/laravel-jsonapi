d3858464c89013ea9dfa2cc389fb417cf395a697
Coen Zimmerman <tabun@xs4all.nl>
2017-06-17 16:38:02 +0200

    JSON-API schema validation error response improved
    now follows JSON-API specs

--- config/jsonapi.php
+++ config/jsonapi.php
@@ -3,6 +3,8 @@
 use Czim\JsonApi\Contracts\Support\Error\ErrorDataInterface;
 use Czim\JsonApi\Encoder\Transformers\ErrorDataTransformer;
 use Czim\JsonApi\Encoder\Transformers\ModelRelationshipTransformer;
+use Czim\JsonApi\Encoder\Transformers\ValidationExceptionTransformer;
+use Czim\JsonApi\Exceptions\JsonApiValidationException;
 use Czim\JsonApi\Support\Resource\RelationshipTransformData;
 
 return [
@@ -66,6 +68,10 @@ return [
         // The default datetime format to apply to date attributes.
         'default-datetime-format' => 'c',
 
+        // If this is enabled, multiple errors for a single input key are string-concatenated
+        // in a single JSON-API error object. If disabled, each error has its own error object.
+        'group-validation-errors-by-key' => false,
+
         'links' => [
 
             // Whether the 'relationships' link should be included where possible.
@@ -104,8 +110,9 @@ return [
         // The TransformerFactory will use this to instantiate transformers based on an is_a() match
         // on given content, if no standard match was found.
         'map' => [
-            ErrorDataInterface::class        => ErrorDataTransformer::class,
-            RelationshipTransformData::class => ModelRelationshipTransformer::class,
+            JsonApiValidationException::class => ValidationExceptionTransformer::class,
+            ErrorDataInterface::class         => ErrorDataTransformer::class,
+            RelationshipTransformData::class  => ModelRelationshipTransformer::class,
             // \Your\ClassHere::class => \Your\Transformer\ClassHere::class
         ],
     ],
---
+++ src/Encoder/Transformers/ValidationExceptionTransformer.php
@@ -0,0 +1,105 @@
+<?php
+namespace Czim\JsonApi\Encoder\Transformers;
+
+use Czim\JsonApi\Contracts\Support\Error\ErrorDataInterface;
+use Czim\JsonApi\Exceptions\JsonApiValidationException;
+use Czim\JsonApi\Support\Error\ErrorData;
+use Exception;
+use InvalidArgumentException;
+
+class ValidationExceptionTransformer extends ErrorDataTransformer
+{
+
+    /**
+     * Transforms given data.
+     *
+     * @param mixed $exception
+     * @return array
+     */
+    public function transform($exception)
+    {
+        if ( ! ($exception instanceof JsonApiValidationException)) {
+            throw new InvalidArgumentException("ValidationExceptionTransformer expects JsonApiValidationException instance");
+        }
+
+        $data = $this->convertExceptionToErrorData($exception);
+
+        return parent::transform($data);
+    }
+
+    /**
+     * Converts exception instance to ErrorDataInterface instance
+     *
+     * @param JsonApiValidationException $exception
+     * @return ErrorDataInterface[]
+     */
+    protected function convertExceptionToErrorData(JsonApiValidationException $exception)
+    {
+        $errorsData = [];
+
+        $prefix = $exception->getPrefix();
+
+        foreach ($exception->getErrors() as $key => $errors) {
+
+            if (config('jsonapi.transform.group-validation-errors-by-key')) {
+
+                $errorsData[] = new ErrorData([
+                    'status' => (string) $this->getStatusCode($exception),
+                    'code'   => (string) $exception->getCode(),
+                    'title'  => $exception->getMessage(),
+                    'detail' => implode("\n", $errors),
+                    'source' => ['pointer' => $prefix . $key],
+                ]);
+                continue;
+            }
+
+            foreach ($errors as $error) {
+
+                $errorsData[] = new ErrorData([
+                    'status' => (string) $this->getStatusCode($exception),
+                    'code'   => (string) $exception->getCode(),
+                    'title'  => $exception->getMessage(),
+                    'detail' => $error,
+                    'source' => ['pointer' => $prefix . $key],
+                ]);
+            }
+        }
+
+        return $errorsData;
+    }
+
+    /**
+     * @param Exception $exception
+     * @return int|mixed
+     */
+    protected function getStatusCode(Exception $exception)
+    {
+        // special case: fully formed response exception (laravel 5.2 validation)
+        if (is_a($exception, \Illuminate\Http\Exceptions\HttpResponseException::class)) {
+            /** @var \Illuminate\Http\Exceptions\HttpResponseException $exception */
+            return $exception->getResponse()->getStatusCode();
+        }
+
+        $mapping = config('jsonapi.exceptions.status', []);
+
+        if (array_key_exists(get_class($exception), $mapping)) {
+            return $mapping[ get_class($exception) ];
+        }
+
+        if (method_exists($exception, 'getStatusCode')) {
+            return $exception->getStatusCode();
+        }
+
+        return 500;
+    }
+
+    /**
+     * @param Exception $exception
+     * @return string
+     */
+    protected function getTitle(Exception $exception)
+    {
+        return ucfirst(snake_case(class_basename($exception), ' '));
+    }
+
+}
